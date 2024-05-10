f3c113d08f0b595d9650d84119bc7f2a951c743f
Furkan <furkanresul@gmail.com>
2024-05-10 15:22:10 +0300

    displays images

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,59 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const App = () => {
-  return <div>hi</div>;
+  const [data, setData] = useState(null);
+  const options = {
+    method: 'GET',
+    url: 'https://opencritic-api.p.rapidapi.com/game',
+    params: {
+      platforms: 'all',
+      sort: 'date',
+      order: 'asc',
+      skip: '20',
+    },
+    headers: {
+      'X-RapidAPI-Key': 'fc131178cdmsh68f34188b659107p19aedfjsnae0ff1c52c70',
+      'X-RapidAPI-Host': 'opencritic-api.p.rapidapi.com',
+    },
+  };
+
+  const fetchData = async () => {
+    try {
+      const dataString = localStorage.getItem('myData');
+
+      if (dataString) {
+        const data = JSON.parse(dataString);
+        setData(data);
+        console.log(data);
+      } else {
+        const response = await axios.request(options);
+        const newDataString = JSON.stringify(response.data);
+        localStorage.setItem('myData', newDataString);
+        setData(response.data);
+        console.log(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data.map((item, index) => (
+        <div key={index} className="card">
+          <img
+            src={'https://img.opencritic.com/' + item.images?.banner?.sm}
+            alt={item.name}
+          />
+          <h2>{item.name}</h2>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default App;
